36e0530f583da14a1af0465b37e0d9c8c063e578
Jinay Dagli <jinaydagli@gmail.com>
2023-04-09 21:28:12 +0530

    Update data_visualisation.py

--- data/data_visualisation.py
+++ data/data_visualisation.py
@@ -1,20 +1,19 @@
-from matplotlib.image import imread
 import matplotlib.pyplot as plt
 import os
 
 # Path: data\data_visualisation.py
 
-def show_images(folder_key):
-    path = "data/images/" + folder_key + "/"
+def show_images():
+    path = "data/images/" 
     i = 0
     for filename in os.listdir(path):
-        plt.subplot(330 + 1 + i)
-        img = imread(path + filename)      
-        plt.imshow(img)
-        i += 1
-        if i == 9:
-            break
+        if(filename.startswith("jackal")):
+            plt.subplot(330 + 1 + i)
+            img = imread(path + filename)      
+            plt.imshow(img)
+            i += 1
+            if i == 9:
+                break
     plt.show()
 
-show_images("jackal")
-show_images("nilgai")
+show_images()
